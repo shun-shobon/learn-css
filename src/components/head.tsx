18f2d8420445c55ed24819e838eaf404428617ac
shun-shobon <shun.shobon@gmail.com>
2020-08-03 14:52:08 +0900

    feat: add title

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-const Head: React.FC = () => {
+type HeadProps = {
+  title?: string;
+};
+
+const Head: React.FC<HeadProps> = ({ title }) => {
+  const fullTitle = title ? `${title} | CSS Learn` : `CSS Learn`;
+
   return (
     <Helmet htmlAttributes={{ lang: "ja" }}>
       <meta charSet={"utf-8"} />
+      <title>{fullTitle}</title>
     </Helmet>
   );
 };
