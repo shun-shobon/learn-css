4fe1d34e226349dbd28b1a153d70f1a20fb9ea76
shun-shobon <shun.shobon@gmail.com>
2020-08-03 14:49:06 +0900

    feat: add char set

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 const Head: React.FC = () => {
-  return <Helmet htmlAttributes={{ lang: "ja" }} />;
+  return (
+    <Helmet htmlAttributes={{ lang: "ja" }}>
+      <meta charSet={"utf-8"} />
+    </Helmet>
+  );
 };
 
 export default Head;
